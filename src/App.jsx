2a00c16346362af6e1ce6352c16417292307b849
ifshad <shoronshark@gmail.com>
2024-01-11 21:30:11 +0600

    options loaded dynamically from api

--- src/App.jsx
+++ src/App.jsx
@@ -16,15 +16,15 @@ const App = () => {
   return (
     <div className="min-h-screen flex justify-center items-center text-center">
       <div>
-      <h2>Total Country: {countries.length}</h2>
-        {
-          countries.map(
-            (country , index) => 
-            <div key={country.cca2}>
-              <p>{`${index+1}.${country.name.common}`}</p>
-            </div>
-          )
-        }
+        <h2>Total Country: {countries.length}</h2>
+        <select className="select select-bordered w-full max-w-xs">
+          <option disabled selected>Who shot first?</option>
+          {
+            countries.map((country) =>
+            <option key={country.cca2}>{country.name.common}</option>
+            )
+          }
+        </select>
       </div>
     </div>
   );
